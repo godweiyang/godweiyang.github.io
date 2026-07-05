7a57163fd3da9036e2f6fa7087dc8eb1b959505a
godweiyang <792321264@qq.com>
2026-07-05 20:21:53 +0800

    Site updated: 2026-07-05 20:21:45

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,16 +1,39 @@
 // 代码块语言识别
 
 $(function () {
-  var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>')
-
-  $('pre').after($highlight_lang)
   $('pre').each(function () {
     var code_language = $(this).attr('class')
-
     if (!code_language) {
-      return true
+      var $figure = $(this).closest('figure.highlight')
+      if ($figure.length) {
+        code_language = $figure.attr('class')
+      }
     }
-    var lang_name = code_language.replace('line-numbers', '').trim().replace('language-', '').trim()
+
+    if (!code_language) return true
+
+    var classes = code_language.split(/\s+/).filter(Boolean)
+    var lang_name = null
+
+    classes.forEach(function (c) {
+      if (lang_name) return
+      if (c.indexOf('language-') === 0) {
+        lang_name = c.replace('language-', '').trim()
+      }
+    })
+
+    if (!lang_name) {
+      classes.forEach(function (c) {
+        if (lang_name) return
+        if (c === 'highlight' || c === 'line-numbers') return
+        lang_name = c.trim()
+      })
+    }
+
+    if (!lang_name) return true
+
+    var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>')
+    $(this).after($highlight_lang)
 
     // 首字母大写
     lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1)
